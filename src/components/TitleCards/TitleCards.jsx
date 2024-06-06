c2cebb428839828ab923dfdadadb954a91378408
ABHIRAJ T R <abhirajtr007@gmail.com>
2024-06-06 13:42:53 +0530

    Add movie cards for displaying movie titles and visuals

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
-const TitleCards = () => {
+
+const TitleCards = ({ title }) => {
+  const cardsRef = useRef();
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel)
+  })
   return (
     <div className='titlecards'>
-
+      <h2>{title ? title : "Polpular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return <div className='card' key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
